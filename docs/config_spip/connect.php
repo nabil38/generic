515e598ae4ee174f32f1b03195ce7925a67d9e98
Nabil DJIDI <public@djidi.com>
2017-10-02 11:42:48 +0100

    maj fichier de connexion

--- docs/config_spip/connect.php
+++ docs/config_spip/connect.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
-define('_MYSQL_SET_SQL_MODE',true);
-$GLOBALS['spip_connect_version'] = 0.7;
-spip_connect_db('MYSQL_ADRESSE','','MYSQL_USER','MYSQL_PASSWORD','MYSQL_BASE','mysql', 'spip','');
+defined('_MYSQL_SET_SQL_MODE') || define('_MYSQL_SET_SQL_MODE',true);
+$GLOBALS['spip_connect_version'] = 0.8;
+spip_connect_db('MYSQL_ADRESSE','','MYSQL_USER','MYSQL_PASSWORD','MYSQL_BASE','mysql', 'spip','','utf8');
 ?>
