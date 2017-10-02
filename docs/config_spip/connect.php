4cf2f90bd5d37fba47265584d2916e43e39b1efe
Nabil DJIDI <public@djidi.com>
2017-10-02 15:40:23 +0100

    charset issue

--- docs/config_spip/connect.php
+++ docs/config_spip/connect.php
@@ -2,5 +2,5 @@
 if (!defined("_ECRIRE_INC_VERSION")) return;
 defined('_MYSQL_SET_SQL_MODE') || define('_MYSQL_SET_SQL_MODE',true);
 $GLOBALS['spip_connect_version'] = 0.8;
-spip_connect_db('MYSQL_ADRESSE','','MYSQL_USER','MYSQL_PASSWORD','MYSQL_BASE','mysql', 'spip','','utf8');
+spip_connect_db('MYSQL_ADRESSE','','MYSQL_USER','MYSQL_PASSWORD','MYSQL_BASE','mysql', 'spip','','');
 ?>
